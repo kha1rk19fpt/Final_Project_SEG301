261f4c01fd7624c918bc7fc6756ba6d2b393cf7c
khaichimdai <khaitrinh2005@gmail.com>
2026-06-13 22:58:42 +0700

    md format res, update crawl table

--- src/backend/utils/md_formatter.py
+++ src/backend/utils/md_formatter.py
@@ -4,4 +4,25 @@ import re
 
 class MarkdownFormatter:
     def __init__(self, OUTPUT_DIR: str):
-        
+        self.outputdir = OUTPUT_DIR
+        os.makedirs(self.outputdir, exist_ok=True)
+    def save_to_markdown(self, title:str, url:str, content:str, tables_str:str, chunk_idx:int)-> str:
+        safe_title =  re.sub(r'[\\/*?"<>|]', "", title).replace(" ", "_")
+        safe_title = safe_title[:50]
+        file_name = f"{safe_title}_chunk_{chunk_idx}.md"
+        file_path = os.path.join(self.outputdir, file_name)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(f"{title}")
+            f.write(f"URL: {url}")
+            f.write("Content:")
+            f.write(f"{content}")
+
+            if tables_str:
+                try:
+                    tables_dict = json.load(tables_str)
+                    if tables_dict:
+                        f.write("Table data:")
+                        f.write(json.dump(tables_dict, ascii=False, indent=4))
+                except json.JSONDecodeError:
+                    pass
+        return file_path
--- wiki_bot/wiki_bot/spiders/wiki_spider.py
+++ wiki_bot/wiki_bot/spiders/wiki_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 from urllib.parse import urljoin
+import pandas as pd
+from io import StringIO
 
 class WikipediaSpider(scrapy.Spider):
     name = "wiki_spider"
@@ -15,16 +17,32 @@ class WikipediaSpider(scrapy.Spider):
         self.scraped_count = 0
         
     def parse(self, response):
-        title = response.css("h1#firstHeading::text").get()
+        #Get title
+        title_element = response.css("span.mw-page-title-main::text").get()
+        if not title_element:
+            title_element = response.css("h1#firstHeading *::text").get()
+        title = title_element.strip() if title_element else "Unknown title"
+        #Get content
         paragraphs = response.css("div.mw-parser-output p *::text, div.mw-parser-output p::text").getall()
         full_text = " ".join([p.strip() for p in paragraphs if p.strip()])
+        #Get table
+        table_dict = []
+        try:
+            html_content = response.body.decode(response.encoding)
+            dataframe = pd.read_html(StringIO(html_content))
+            for df in dataframe:
+                df = df.dropna(how='all').fillna("").astype(str)
+                table_dict.append(df.to_dict(orient='records'))
+        except Exception:
+            pass
         if full_text and len(full_text) > 100:
             self.scraped_count += 1
             yield {
                 'id': self.scraped_count,
                 'title': title,
                 'url': response.url,
-                'text': full_text
+                'text': full_text,
+                'tables': table_dict
             }
         all_links = response.css("div.mw-parser-output a::attr(href)").getall()
         for link in all_links:
