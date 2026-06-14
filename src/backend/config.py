78ad5c47a2a187407b8a2caaaed913fc83c66069
khaichimdai <khaitrinh2005@gmail.com>
2026-06-14 19:06:49 +0700

    update dir data

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,8 +1,8 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-JSON_PATH = os.path.join("data", "raw", "wikipedia_dataset.json")
-VDB_PATH = os.path.join("data", "vector")
+JSON_PATH = os.path.join(BASE_DIR, "data", "raw", "wikipedia_dataset.json")
+VDB_PATH = os.path.join(BASE_DIR, "data", "vector")
 
 EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 COLLECTION_NAME = "wiki_ai_english"
