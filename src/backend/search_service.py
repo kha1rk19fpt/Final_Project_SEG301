44cd9c41698a562bb70fab5e2543ccabbf79fc9e
khaichimdai <khaitrinh2005@gmail.com>
2026-06-14 22:43:48 +0700

    update search service and build vector db

--- src/backend/search_service.py
+++ src/backend/search_service.py
@@ -1,7 +1,10 @@
+import os
 import time
 import chromadb
 from chromadb.utils import embedding_functions
-from src.backend.config import VDB_PATH, EMBEDDING_MODEL_NAME, COLLECTION_NAME
+from src.backend.config import VDB_PATH, EMBEDDING_MODEL_NAME, COLLECTION_NAME, BASE_DIR
+from src.backend.utils.query_extraction import QueryExtraction
+from src.backend.utils.md_formatter import MarkdownFormatter
 
 class SearchService:
     def __init__(self):
@@ -11,28 +14,42 @@ class SearchService:
             name = COLLECTION_NAME,
             embedding_function= self.ef
         )
+        self.extraction = QueryExtraction()
+        md_output_dir = os.path.join(BASE_DIR, 'data', 'markdown_docs')
+        self.md_formatter = MarkdownFormatter(OUTPUT_DIR=md_output_dir)
 
     def search(self, query:str, top_k=5):
         start_time = time.time()
+        extracted_text = self.extraction.extract(query)
+        search_kw = extracted_text.get("search_keywords", query)
         res = self.collection.query(
-            query_texts= query,
+            query_texts= search_kw,
             n_results= top_k
         )
-        end_time = time.time()
-        processed_time_ms = round((end_time - start_time) * 1000, 2)
+        
 
         formatted_res = []
         if res["documents"] and len(res["documents"][0]) > 0:
             for i in range(len(res["documents"][0])):
+                doc_title = res["metadatas"][0][i].get('title', 'Unknow')
+                doc_url = res["metadatas"][0][i].get('url', "")
+                doc_tables = res["metadatas"][0][i].get('tables', "")
+                doc_content = res["documents"][0][i]
+                md_path = self.md_formatter.save_to_markdown(title=doc_title,url=doc_url,tables_str=doc_tables,content=doc_content,chunk_idx=i)
                 formatted_res.append({
-                        "title": res["metadatas"][0][i]["title"],
-                        "url" : res["metadatas"][0][i]["url"],
-                        "content" : res["documents"][0][i],
-                        "distance_score": res["distances"][0][i]
-                    })
+                    "title": doc_title,
+                    "url": doc_url,
+                    "content": md_path,
+                    "distance_score": res["distances"][0][i]
+                })
+                
+        end_time = time.time()
+        processed_time_ms = round((end_time - start_time) * 1000, 2)
         return {
-            "query" :query,
-            "processing time" : processed_time_ms,
-            "total result" : len(formatted_res),
-            "data" : formatted_res
+            "query": query,
+            "extracted_context": extracted_text.get("context", ""),
+            "optimized_search_keyword": search_kw,
+            "processing time ms": processed_time_ms,
+            "total result": len(formatted_res),
+            "data": formatted_res
         }
--- src/indexing/build_vector_db.py
+++ src/indexing/build_vector_db.py
@@ -1,21 +1,19 @@
 import json 
-import os
 import chromadb
 from chromadb.utils import embedding_functions
-from src.backend.config import EMBEDDING_MODEL_NAME, COLLECTION_NAME
-
-#set up dir
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-JSON_PATH = os.path.join("data", "raw", "wikipedia_dataset.json")
-VDB_PATH = os.path.join("data", "vector")
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_huggingface import HuggingFaceEmbeddings
+from src.backend.config import EMBEDDING_MODEL_NAME, COLLECTION_NAME, VDB_PATH, JSON_PATH
 
 def main():
-    # Itializing Vector DB & AI model
+    # Itializing Vector DB & AI model &Semantic chunker
     client  = chromadb.PersistentClient(path = VDB_PATH)
     ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name= EMBEDDING_MODEL_NAME)
+    semantic_embedding = HuggingFaceEmbeddings(model_name= EMBEDDING_MODEL_NAME)
+    text_semantic_chunk = SemanticChunker(semantic_embedding, breakpoint_threshold_type="percentile")
     try:
         client.delete_collection(name= COLLECTION_NAME)
-    except:
+    except Exception:
         pass
     collection = client.create_collection(name=COLLECTION_NAME, embedding_function=ef)
 
@@ -25,22 +23,28 @@ def main():
     metadatas = []
     ids = []
 
-    #chunking
+    #Semantic chunking
     chunk_id = 0
     for article in data:
-        title = article.get("title") or "No Title"
+        title = article.get("title") or "Unknow title"
         url = article.get("url") or ""
         text = article.get("text") or ""
+        tables = article.get("tables") or []
 
         if not text:
             continue
-
-        paragraphs = text.split("\n")
-        for p in paragraphs:
-            p = p.strip()
-            if len(p) > 150:
-                docs.append(p)
-                metadatas.append({"title": str(title), "url":str(url)})
+        
+        semantic_chunking = text_semantic_chunk.create_documents([text])
+        for sc in semantic_chunking:
+            chunk_text = sc.page_content.strip()
+            if len(chunk_text) > 50:
+                docs.append(chunk_text)
+                table_str = json.dumps(tables, ensure_ascii=False) if tables else ""
+                metadatas.append({
+                    'title': str(title),
+                    'url': str(url),
+                    "tables": table_str
+                })
                 ids.append(f"chunk_{chunk_id}")
                 chunk_id += 1
 
