25c6ebda2315db39b4a11c5b1e81ab5d5242f3fc
khaichimdai <khaitrinh2005@gmail.com>
2026-06-15 21:17:24 +0700

    fix direction config and update readme

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-JSON_PATH = os.path.join(BASE_DIR, "data", "raw", "wikipedia_dataset.json")
+JSON_PATH = os.path.join(BASE_DIR, "data", "raw", "wiki_crawler_dataset.json")
 VDB_PATH = os.path.join(BASE_DIR, "data", "vector")
 
 EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
