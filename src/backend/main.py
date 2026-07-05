f11145c977b01d57dc77493fbe5a483f59d4b9da
khaichimdai <khaitrinh2005@gmail.com>
2026-07-05 10:08:49 +0700

    Connect front-end with back-end

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,10 @@
+import os
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from src.backend.search_service import SearchService
-from src.backend.config import DEFAULT_TOP_K
- 
+from src.backend.config import DEFAULT_TOP_K, BASE_DIR
+
 app = FastAPI(
     title="WikiSearch API",
     description="API for searching — PRJ SEG",
@@ -15,23 +17,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
- 
+
 search_service = SearchService()
- 
- 
-@app.get("/")
-def read_root():
-    return {"status": "Backend is running!", "version": "1.1.0"}
- 
- 
 @app.get("/search")
 def search_wiki(
-    q: str = Query(..., description="Type a natural query'"),
+    q: str = Query(..., description="Type a natural query"),
     k: int = Query(DEFAULT_TOP_K, ge=1, le=20, description="Default: 5 documents"),
 ):
     return search_service.search(query=q, top_k=k)
- 
- 
+
 @app.get("/health")
 def health_check():
     index_size = len(search_service._full_text_index)
@@ -39,9 +33,12 @@ def health_check():
         collection_count = search_service.collection.count()
     except Exception as e:
         return {"status": "error", "detail": str(e)}
- 
+
     return {
         "status": "ok",
         "collection_count": collection_count,
         "full_text_index_size": index_size,
     }
+
+FRONTEND_DIR = os.path.join(BASE_DIR, "src", "frontend")
+app.mount("/", StaticFiles(directory=FRONTEND_DIR, html=True), name="frontend")
