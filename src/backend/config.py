4cfb43a8d4714819730efdfe2ac331c090171ba7
khaichimdai <khaitrinh2005@gmail.com>
2026-07-05 10:12:15 +0700

    split into System and evaluation flows

--- src/backend/config.py
+++ src/backend/config.py
@@ -6,9 +6,9 @@ if DATA_PROFILE == 'eval':
     JSON_PATH = os.path.join(BASE_DIR, "data","eval", "raw", "wiki_crawler_dataset.json")
     VDB_PATH = os.path.join(BASE_DIR, "data", "eval", "vector")
 else:
-    JSON_PATH = os.path.join(BASE_DIR, "data", "raw", "wiki_crawler_dataset.json")
+    JSON_PATH = os.path.join(BASE_DIR, "data", "raw", "wiki_crawler_dataset.jsonl")
     VDB_PATH = os.path.join(BASE_DIR, "data", "vector")
 
-EMBEDDING_MODEL_NAME = "BAAI/bge-small-en-v1.5" #replace all-MiniLM-L6-v2
+EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 COLLECTION_NAME = "wiki_ai_english"
 DEFAULT_TOP_K = 5
